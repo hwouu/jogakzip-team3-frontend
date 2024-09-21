d6a6f6973ab8c1ce423b08796a7179affdc2abee
Hyunwoo <nhw3990@kau.kr>
2024-09-21 20:04:20 +0900

    Feat #2: 그룹 생성 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import GroupList from './pages/GroupList';
-import CreatePost from './pages/CreatePost';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';  // Home 페이지 임포트
+import GroupList from './pages/GroupList';  // 그룹 리스트 페이지 임포트
+import CreatePost from './pages/CreatePost';  // 게시글 작성 페이지 임포트
+import CreateGroup from './pages/CreateGroup';  // 새로 만든 그룹 생성 페이지 임포트
 
 function App() {
   return (
@@ -11,12 +15,15 @@ function App() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/groups">Groups</Link></li>
           <li><Link to="/create-post">Create Post</Link></li>
+          <li><Link to="/create-group">Create Group</Link></li> {/* 그룹 생성 링크 추가 */}
         </ul>
       </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/groups" element={<GroupList />} />
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/create-group" element={<CreateGroup />} /> {/* 그룹 생성 페이지 라우트 추가 */}
       </Routes>
     </Router>
   );
