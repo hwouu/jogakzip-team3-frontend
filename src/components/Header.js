1fb421e3d75d701ef7c7a338450da6ca6cd835b0
Hyunwoo <nhw3990@kau.kr>
2024-09-29 20:51:51 +0900

    fix: 로고 클릭 시 이동 경로 변경 (/groups)

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="header">
       <div className="header-content">
         {/* 로고에 링크를 걸어서 클릭하면 기본 페이지로 이동 */}
-        <Link to="/">
+        <Link to="/groups">
           <img src="/jogakzip1.svg" alt="조각집 로고" className="logo" />
         </Link>
       </div>
