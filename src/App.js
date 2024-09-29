52fc84076eddc2c07fc0151aeb773c1937a730a2
Hyunwoo <nhw3990@kau.kr>
2024-09-29 14:38:34 +0900

    fix: 라우트 순서 재정렬

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header"; // 헤더 임포트
 import GroupList from "./pages/Group/GroupList"; // 그룹 리스트 페이지 임포트
-import CreatePost from "./pages/Post/CreatePost"; // 게시글 작성 페이지 임포트
 import CreateGroup from "./pages/Group/CreateGroup"; // 그룹 생성 페이지 임포트
 import GroupDetail from "./pages/Group/GroupDetail"; // 그룹 상세 페이지 임포트
-import Header from "./components/Header"; // 헤더 임포트
+import CreateMemory from "./pages/Memory/CreateMemory"; // 게시글 작성 페이지 임포트
 
 function App() {
   return (
@@ -18,12 +18,12 @@ function App() {
         {/* 기본 페이지를 GroupList로 설정 */}
         <Route path="/groups" element={<GroupList />} />{" "}
         {/* 그룹 목록 페이지 */}
-        <Route path="/create-post" element={<CreatePost />} />{" "}
-        {/* 게시글 작성 페이지 */}
         <Route path="/create-group" element={<CreateGroup />} />{" "}
         {/* 그룹 생성 페이지 */}
         <Route path="/group/:id" element={<GroupDetail />} />{" "}
         {/* 그룹 상세 페이지 */}
+        <Route path="/group/:id/create-memory" element={<CreateMemory />} />{" "}
+        {/* 특정 그룹에 대한 게시글 작성 페이지 */}
       </Routes>
     </Router>
   );
